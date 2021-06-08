829f4f1d6e370376a6f44a9005f35f3e7ac0d1ad
Carlos Henrique de Lima <68131444+carlos334lima@users.noreply.github.com>
2021-06-08 00:26:19 -0300

    web started

---
+++ web/src/pages/SignIn/index.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
+import React, { useRef, useCallback } from 'react';
+
+/* import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import { Link, useHistory } from 'react-router-dom';
+import * as Yup from 'yup';
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
+import logoImg from '../../assets/logo.svg';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import getValidationErrors from '../../utils/getValidationErros'; */
+
+import { Container } from './styles';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <h1>SignIN</h1>
+  </Container>
+);
+
+export default SignIn;
---
+++ web/src/pages/SignUp/index.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable linebreak-style */
+import React from 'react';
+
+/* import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import { Link, useHistory } from 'react-router-dom';
+import logoImg from '../../assets/logo.svg';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import { useToast } from '../../hooks/toast';
+
+import getValidationErrors from '../../utils/getValidationErros';
+import api from '../../services/api'; */
+
+import { Container } from './styles';
+
+const SignUp: React.FC = () => <Container />;
+
+export default SignUp;
