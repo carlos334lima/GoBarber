11b3c5a0490343d3edd040984a4f8b259b51f091
Carlos Henrique de Lima <68131444+carlos334lima@users.noreply.github.com>
2021-06-17 18:53:27 -0300

    Route configuration

---
+++ mobile/src/routes/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SignIn from '../screens/SignIn';
+import SignUp from '../screens/SignUp';
+
+const { Screen, Navigator } = createStackNavigator();
+
+const AuthRoutes: React.FC = () => {
+  return (
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: { backgroundColor: '#312e38' },
+      }}
+    >
+      <Screen name="SignIn" component={SignIn} />
+      <Screen name="SignUp" component={SignUp} />
+    </Navigator>
+  );
+};
+
+export default AuthRoutes;
